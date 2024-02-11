2cb07e46d18c13755ac7e5a931a296d3d158ca19
Arthur <arthur.rodriguez640@gmail.com>
2024-02-11 15:18:12 +0100

    pue la merde

--- back/index.js
+++ back/index.js
@@ -7,7 +7,7 @@ const apiQuery = require('./queryManagers/api.js')
 /* The http module contains a createServer function, which takes one argument, which is the function that
 ** will be called whenever a new request arrives to the server.
  */
-http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
     // First, let's check the URL to see if it's a REST request or a file request.
     // We will remove all cases of "../" in the url for security purposes.
     let filePath = request.url.split("/").filter(function(elem) {
@@ -28,4 +28,19 @@ http.createServer(function (request, response) {
         response.end(`Something in your request (${request.url}) is strange...`);
     }
 // For the server to be listening to request, it needs a port, which is set thanks to the listen function.
-}).listen(8000);
+});
+
+const socketIo = require('../front/js/socket.io.js');
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('dataSent', (data) => {
+        console.log(data);
+    });
+});
+  
+server.listen(8000, () => {
+    console.log('listening on *:8000');
+  });
