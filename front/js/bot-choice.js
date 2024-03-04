87a7474e468a70be50a3bd6989bbc097665b860b
Arthur <arthur.rodriguez640@gmail.com>
2024-03-04 01:27:55 +0100

    le fix du pipi

---
+++ front/js/bot-choice.js
@@ -0,0 +1,36 @@
+const socket = io("/api/game");
+
+const AI0 = document.getElementById("AI0");
+const AI1 = document.getElementById("AI1");
+const AI2 = document.getElementById("AI2");
+
+let difficulty;
+
+AI0.addEventListener("click", () => {
+  difficulty = 0;
+  socket.emit("createGameAI", {
+    difficulty: difficulty,
+    token: localStorage.getItem("token"),
+  });
+});
+
+AI1.addEventListener("click", () => {
+  difficulty = 1;
+  socket.emit("createGameAI", {
+    difficulty: difficulty,
+    token: localStorage.getItem("token"),
+  });
+});
+
+AI2.addEventListener("click", () => {
+  difficulty = 2;
+  socket.emit("createGameAI", {
+    difficulty: difficulty,
+    token: localStorage.getItem("token"),
+  });
+});
+
+socket.on("gameCreated", (game) => {
+  window.location.href =
+    "ai-game.html?difficulty=" + difficulty + "id=" + game._id;
+});
