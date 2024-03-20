bb532cd7d61f70256372d0ee21810a161dd3eafe
Marc Pinet <52708150+marcpinet@users.noreply.github.com>
2024-03-20 21:48:56 +0100

    fix: api.js (#1)

--- back/query-managers/api.js
+++ back/query-managers/api.js
@@ -550,7 +550,9 @@ async function handleFriendRequestPost(request, response, decodedToken) {
       }
 
       // Vérifier si les utilisateurs sont amis
-      const userFriendsAsStrings = user.friends.map((id) => id.toString());
+      const userFriendsAsStrings = currentUser.friends.map((id) =>
+        id.toString(),
+      );
       const friendFriendsAsStrings = friend.friends.map((id) => id.toString());
       let friendId = friendUser._id.toString();
 
